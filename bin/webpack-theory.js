7f25e406e17cdd852302deceb9f08f58514c48d9
nordon.wang <nordon.wang@oyohotels.cn>
2019-07-28 23:32:02 +0800

    原理

--- bin/webpack-theory.js
+++ bin/webpack-theory.js
@@ -1,4 +1,12 @@
 #!/usr/bin/env node
+const path = require('path')
 
-console.log('当通过npm link链接之后，通过oyo-pack指令可以直接打出');
+// console.log('当通过npm link链接之后，通过oyo-pack指令可以直接打出');
 
+// 1. 读取需要打包项目的配置文件
+const config = require(path.resolve('webpack.config.js'))
+// console.log(config)
+
+// 2. 通过面向对象的方式来进行项目推进
+const Compiler = require('../lib/Compiler')
+new Compiler(config).start()
